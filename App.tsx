e0301506e0b4f527979030b6315895c98d284998
shibli21 <syedshiblimahmud@gmail.com>
2021-01-16 19:10:56 +0600

    ✨ add fonts

--- App.tsx
+++ App.tsx
@@ -1,8 +1,29 @@
+import AppLoading from "expo-app-loading";
+import * as Font from "expo-font";
 import { StatusBar } from "expo-status-bar";
-import React from "react";
+import React, { useState } from "react";
 import { StyleSheet, Text, View } from "react-native";
 
+const fetchFonts = async () => {
+  Font.loadAsync({
+    "open-sans": require("./assets/fonts/OpenSans-Regular.ttf"),
+    "open-sans-bold": require("./assets/fonts/OpenSans-Bold.ttf"),
+  });
+};
+
 export default function App() {
+  const [fontLoaded, setFontLoaded] = useState<Boolean>(false);
+
+  if (!fontLoaded) {
+    return (
+      <AppLoading
+        onError={() => console.log("ada")}
+        startAsync={fetchFonts}
+        onFinish={() => setFontLoaded(true)}
+      />
+    );
+  }
+
   return (
     <View style={styles.container}>
       <Text
